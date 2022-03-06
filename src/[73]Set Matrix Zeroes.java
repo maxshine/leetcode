05c8158f8844d5129b231df419b3662ac60cf599
maxshine <maxshine_au@outlook.com>
2022-03-06 14:38:25 +1100

    20220306 solution done with 73

---
+++ src/[73]Set Matrix Zeroes.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int R = matrix.length;
+        int C = matrix[0].length;
+        boolean isCol = false;
+
+        for (int i=0; i<R; i++) {
+            if (matrix[i][0] == 0) {
+                isCol = true;
+            }
+            for (int j=1; j<C; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i=1; i<R; i++) {
+            for (int j=1; j<C; j++) {
+                if (matrix[i][0]==0 || matrix[0][j]==0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int i=1; i<C; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (isCol) {
+            for (int i=0; i<R; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
