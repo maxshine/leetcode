07ac839fb5062c9dd5014cedfc0e318048b1300e
maxshine <maxshine_au@outlook.com>
2022-03-19 15:58:43 +1100

    20220319 solution done with 85

---
+++ src/[85]Maximal Rectangle.java
@@ -0,0 +1,112 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+class Solution {
+    public int maximalRectangleDPWidth(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+        int maxarea = 0;
+        int[][] dp = new int[matrix.length][matrix[0].length];
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if (matrix[i][j] == '1'){
+
+                    // compute the maximum width and update dp with it
+                    dp[i][j] = j == 0? 1 : dp[i][j-1] + 1;
+
+                    int width = dp[i][j];
+
+                    // compute the maximum area rectangle with a lower right corner at [i, j]
+                    for(int k = i; k >= 0; k--){
+                        width = Math.min(width, dp[k][j]);
+                        maxarea = Math.max(maxarea, width * (i - k + 1));
+                    }
+                }
+            }
+        }
+        return maxarea;
+    }
+
+    // Get the maximum area in a histogram given its heights
+    public int leetcode84(int[] heights) {
+        Stack< Integer > stack = new Stack < > ();
+        stack.push(-1);
+        int maxarea = 0;
+        for (int i = 0; i < heights.length; ++i) {
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i])
+                maxarea = Math.max(maxarea, heights[stack.pop()] * (i - stack.peek() - 1));
+            stack.push(i);
+        }
+        while (stack.peek() != -1)
+            maxarea = Math.max(maxarea, heights[stack.pop()] * (heights.length - stack.peek() -1));
+        return maxarea;
+    }
+
+    public int maximalRectangleStack(char[][] matrix) {
+
+        if (matrix.length == 0) return 0;
+        int maxarea = 0;
+        int[] dp = new int[matrix[0].length];
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+
+                // update the state of this row's histogram using the last row's histogram
+                // by keeping track of the number of consecutive ones
+
+                dp[j] = matrix[i][j] == '1' ? dp[j] + 1 : 0;
+            }
+            // update maxarea with the maximum area from this row's histogram
+            maxarea = Math.max(maxarea, leetcode84(dp));
+        }
+        return maxarea;
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0)
+            return 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[] left = new int[n]; // initialize left as the leftmost boundary possible
+        int[] right = new int[n];
+        int[] height = new int[n];
+
+        Arrays.fill(right, n); // initialize right as the rightmost boundary possible
+
+        int maxarea = 0;
+        for (int i = 0; i < m; i++) {
+            int cur_left = 0, cur_right = n;
+            // update height
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1')
+                    height[j]++;
+                else
+                    height[j] = 0;
+            }
+            // update left
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1')
+                    left[j] = Math.max(left[j], cur_left);
+                else {
+                    left[j] = 0;
+                    cur_left = j + 1;
+                }
+            }
+            // update right
+            for (int j = n - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1')
+                    right[j] = Math.min(right[j], cur_right);
+                else {
+                    right[j] = n;
+                    cur_right = j;
+                }
+            }
+            // update area
+            for (int j = 0; j < n; j++) {
+                maxarea = Math.max(maxarea, (right[j] - left[j]) * height[j]);
+            }
+        }
+        return maxarea;
+    }
+}
