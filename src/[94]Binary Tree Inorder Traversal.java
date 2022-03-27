6d3a608ae5445faa6b4f4336dafae6caeb5f2395
maxshine <maxshine_au@outlook.com>
2022-03-28 09:36:14 +1100

    20220328 solution done with 94

---
+++ src/[94]Binary Tree Inorder Traversal.java
@@ -0,0 +1,78 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// @solution-sync:begin
+class Solution {
+    private List<Integer> ans = new LinkedList<Integer>();
+    public List<Integer> inorderTraversalMine(TreeNode root) {
+        if (root != null) {
+            inorderTraversalMine(root.left);
+            ans.add(root.val);
+            inorderTraversalMine(root.right);
+        }
+        return ans;
+    }
+    public List<Integer> inorderTraversalRecursive(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            helper(root.left, res);
+            res.add(root.val);
+            helper(root.right, res);
+        }
+    }
+    public List<Integer> inorderTraversalIterationStack(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+    }
+    public List<Integer> inorderTraversalIterationMorris(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        TreeNode curr = root;
+        TreeNode pre;
+        while (curr != null) {
+            if (curr.left == null) {
+                res.add(curr.val);
+                curr = curr.right; // move to next right node
+            } else { // has a left subtree
+                pre = curr.left;
+                while (pre.right != null) { // find rightmost
+                    pre = pre.right;
+                }
+                pre.right = curr; // put cur after the pre node
+                TreeNode temp = curr; // store cur node
+                curr = curr.left; // move cur to the top of the new tree
+                temp.left = null; // original cur left be null, avoid infinite loops
+            }
+        }
+        return res;
+    }
+}
