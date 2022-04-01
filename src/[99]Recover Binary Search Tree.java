56616012afd65a903656787100cb1902d62558f1
maxshine <maxshine_au@outlook.com>
2022-04-02 09:49:46 +1100

    20220402 solution done with 99

---
+++ src/[99]Recover Binary Search Tree.java
@@ -0,0 +1,166 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// @solution-sync:begin
+class Solution {
+    public void inorder(TreeNode root, List<Integer> nums) {
+        if (root == null) return;
+        inorder(root.left, nums);
+        nums.add(root.val);
+        inorder(root.right, nums);
+    }
+
+    public int[] findTwoSwapped(List<Integer> nums) {
+        int n = nums.size();
+        int x = -1, y = -1;
+        boolean swapped_first_occurrence = false;
+        for(int i = 0; i < n - 1; ++i) {
+            if (nums.get(i + 1) < nums.get(i)) {
+                y = nums.get(i + 1);
+                if (!swapped_first_occurrence) {
+                    // first swap occurrence
+                    x = nums.get(i);
+                    swapped_first_occurrence = true;
+                } else {
+                    // second swap occurrence
+                    break;
+                }
+            }
+        }
+        return new int[]{x, y};
+    }
+
+    public void recover(TreeNode r, int count, int x, int y) {
+        if (r != null) {
+            if (r.val == x || r.val == y) {
+                r.val = r.val == x ? y : x;
+                if (--count == 0) return;
+            }
+            recover(r.left, count, x, y);
+            recover(r.right, count, x, y);
+        }
+    }
+
+    public void recoverTreeInorderTraverseRecursive(TreeNode root) {
+        List<Integer> nums = new ArrayList();
+        inorder(root, nums);
+        int[] swapped = findTwoSwapped(nums);
+        recover(root, 2, swapped[0], swapped[1]);
+    }
+
+    // 2
+    public void swap(TreeNode a, TreeNode b) {
+        int tmp = a.val;
+        a.val = b.val;
+        b.val = tmp;
+    }
+
+    public void recoverTreeInorderIterative(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque();
+        TreeNode x = null, y = null, pred = null;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.removeLast();
+            if (pred != null && root.val < pred.val) {
+                y = root;
+                if (x == null) x = pred;
+                else break;
+            }
+            pred = root;
+            root = root.right;
+        }
+        swap(x, y);
+    }
+
+    //3
+    TreeNode x = null, y = null, pred = null;
+    public void findTwoSwapped(TreeNode root) {
+        if (root == null) return;
+        findTwoSwapped(root.left);
+        if (pred != null && root.val < pred.val) {
+            y = root;
+            if (x == null) x = pred;
+            else return;
+        }
+        pred = root;
+        findTwoSwapped(root.right);
+    }
+
+    public void recoverTreeInorderRecursive(TreeNode root) {
+        findTwoSwapped(root);
+        swap(x, y);
+    }
+
+    // 4 Morris
+    public void recoverTree(TreeNode root) {
+        // predecessor is a Morris predecessor.
+        // In the 'loop' cases it could be equal to the node itself predecessor == root.
+        // pred is a 'true' predecessor,
+        // the previous node in the inorder traversal.
+        TreeNode x = null, y = null, pred = null, predecessor = null;
+
+        while (root != null) {
+            // If there is a left child
+            // then compute the predecessor.
+            // If there is no link predecessor.right = root --> set it.
+            // If there is a link predecessor.right = root --> break it.
+            if (root.left != null) {
+                // Predecessor node is one step left
+                // and then right till you can.
+                predecessor = root.left;
+                while (predecessor.right != null && predecessor.right != root)
+                    predecessor = predecessor.right;
+
+                // set link predecessor.right = root
+                // and go to explore left subtree
+                if (predecessor.right == null) {
+                    predecessor.right = root;
+                    root = root.left;
+                }
+                // break link predecessor.right = root
+                // link is broken : time to change subtree and go right
+                else {
+                    // check for the swapped nodes
+                    if (pred != null && root.val < pred.val) {
+                        y = root;
+                        if (x == null) x = pred;
+                    }
+                    pred = root;
+
+                    predecessor.right = null;
+                    root = root.right;
+                }
+            }
+            // If there is no left child
+            // then just go right.
+            else {
+                // check for the swapped nodes
+                if (pred != null && root.val < pred.val) {
+                    y = root;
+                    if (x == null) x = pred;
+                }
+                pred = root;
+
+                root = root.right;
+            }
+        }
+        swap(x, y);
+    }
+}
