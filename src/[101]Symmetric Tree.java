41b8a9ffb49f1c810ca9e7af79603f12adbdb9a8
maxshine <maxshine_au@outlook.com>
2022-04-04 11:25:29 +1000

    20220404 solution done with 101

---
+++ src/[101]Symmetric Tree.java
@@ -0,0 +1,56 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// @solution-sync:begin
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+    public boolean isMirror(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isMirror(p.left, q.right) && isMirror(p.right, q.left);
+    }
+
+    public boolean isSymmetricIteration(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+    }
+    
+    public void traverse(TreeNode root, List<TreeNode> vals) {
+        if (root == null) {
+            vals.add(null);
+            return;
+        }
+        traverse(root.left, vals);
+        vals.add(root);
+        traverse(root.right, vals);
+    }
+}
