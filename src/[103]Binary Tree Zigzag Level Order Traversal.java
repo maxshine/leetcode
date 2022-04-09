b1885831a30bb4ba2d8aabae0911dc97517f802b
maxshine <maxshine_au@outlook.com>
2022-04-09 10:18:17 +1000

    20220409 solution done with 103

---
+++ src/[103]Binary Tree Zigzag Level Order Traversal.java
@@ -0,0 +1,120 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// @solution-sync:begin
+class Solution {
+    public List<List<Integer>> zigzagLevelOrderMine(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
+        if (root == null) return levels;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int level = 0;
+        while ( !queue.isEmpty() ) {
+            // start the current level
+            levels.add(new ArrayList<Integer>());
+
+            // number of elements in the current level
+            int level_length = queue.size();
+            for(int i = 0; i < level_length; ++i) {
+                TreeNode node = queue.remove();
+
+                // fulfill the current level
+                levels.get(level).add(node.val);
+
+                // add child nodes of the current level
+                // in the queue for the next level
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            if (level % 2 == 1) {
+                Collections.reverse(levels.get(level));
+            }
+            // go to next level
+            level++;
+        }
+        return levels;
+    }
+
+    // BFS
+    public List<List<Integer>> zigzagLevelOrderBFS(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<List<Integer>>();
+        }
+
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+
+        // add the root element with a delimiter to kick off the BFS loop
+        LinkedList<TreeNode> node_queue = new LinkedList<TreeNode>();
+        node_queue.addLast(root);
+        node_queue.addLast(null);
+
+        LinkedList<Integer> level_list = new LinkedList<Integer>();
+        boolean is_order_left = true;
+
+        while (node_queue.size() > 0) {
+            TreeNode curr_node = node_queue.pollFirst();
+            if (curr_node != null) {
+                if (is_order_left)
+                    level_list.addLast(curr_node.val);
+                else
+                    level_list.addFirst(curr_node.val);
+
+                if (curr_node.left != null)
+                    node_queue.addLast(curr_node.left);
+                if (curr_node.right != null)
+                    node_queue.addLast(curr_node.right);
+
+            } else {
+                // we finish the scan of one level
+                results.add(level_list);
+                level_list = new LinkedList<Integer>();
+                // prepare for the next level
+                if (node_queue.size() > 0)
+                    node_queue.addLast(null);
+                is_order_left = !is_order_left;
+            }
+        }
+        return results;
+    }
+    
+    // DFS
+    protected void DFS(TreeNode node, int level, List<List<Integer>> results) {
+        if (level >= results.size()) {
+            LinkedList<Integer> newLevel = new LinkedList<Integer>();
+            newLevel.add(node.val);
+            results.add(newLevel);
+        } else {
+            if (level % 2 == 0)
+                results.get(level).add(node.val);
+            else
+                results.get(level).add(0, node.val);
+        }
+
+        if (node.left != null) DFS(node.left, level + 1, results);
+        if (node.right != null) DFS(node.right, level + 1, results);
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<List<Integer>>();
+        }
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        DFS(root, 0, results);
+        return results;
+    }
+}
