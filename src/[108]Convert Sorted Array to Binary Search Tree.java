da186830e97860ee39d324ed5ca3e2a8add477c4
maxshine <maxshine_au@outlook.com>
2022-04-17 10:13:56 +1000

    20220417 solution done with 108

---
+++ src/[108]Convert Sorted Array to Binary Search Tree.java
@@ -0,0 +1,68 @@
+import java.util.Random;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// @solution-sync:begin
+class Solution {
+    int[] nums;
+    Random rand = new Random();
+
+    public TreeNode helper0(int left, int right) {
+        if (left > right) return null;
+
+        // always choose left middle node as a root
+        int p = (left + right) / 2;
+
+        // preorder traversal: node -> left -> right
+        TreeNode root = new TreeNode(nums[p]);
+        root.left = helper0(left, p - 1);
+        root.right = helper0(p + 1, right);
+        return root;
+    }
+
+    public TreeNode helper1(int left, int right) {
+        if (left > right) return null;
+
+        // always choose right middle node as a root
+        int p = (left + right) / 2;
+        if ((left + right) % 2 == 1) ++p;
+
+        // preorder traversal: node -> left -> right
+        TreeNode root = new TreeNode(nums[p]);
+        root.left = helper1(left, p - 1);
+        root.right = helper1(p + 1, right);
+        return root;
+    }
+
+
+
+    public TreeNode helper(int left, int right) {
+        if (left > right) return null;
+
+        // choose random middle node as a root
+        int p = (left + right) / 2;
+        if ((left + right) % 2 == 1) p += rand.nextInt(2);
+
+        // preorder traversal: node -> left -> right
+        TreeNode root = new TreeNode(nums[p]);
+        root.left = helper(left, p - 1);
+        root.right = helper(p + 1, right);
+        return root;
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        this.nums = nums;
+        return helper1(0, nums.length - 1);
+    }
+}
