d0c5ca295822896820692f3c7c0bfb232a92fd2c
maxshine <maxshine_au@outlook.com>
2022-04-25 08:36:08 +1000

    20220425 solution done with 113

---
+++ src/[113]Path Sum II.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// @solution-sync:begin
+class Solution {
+    private void recurseTree(TreeNode node, int remainingSum, List<Integer> pathNodes, List<List<Integer>> pathsList) {
+
+        if (node == null) {
+            return;
+        }
+
+        // Add the current node to the path's list
+        pathNodes.add(node.val);
+
+        // Check if the current node is a leaf and also, if it
+        // equals our remaining sum. If it does, we add the path to
+        // our list of paths
+        if (remainingSum == node.val && node.left == null && node.right == null) {
+            pathsList.add(new ArrayList<>(pathNodes));
+        } else {
+
+            // Else, we will recurse on the left and the right children
+            this.recurseTree(node.left, remainingSum - node.val, pathNodes, pathsList);
+            this.recurseTree(node.right, remainingSum - node.val, pathNodes, pathsList);
+        }
+
+        // We need to pop the node once we are done processing ALL of it's
+        // subtrees.
+        pathNodes.remove(pathNodes.size() - 1);
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> pathsList = new ArrayList<List<Integer>>();
+        List<Integer> pathNodes = new ArrayList<Integer>();
+        this.recurseTree(root, sum, pathNodes, pathsList);
+        return pathsList;
+    }
+}
