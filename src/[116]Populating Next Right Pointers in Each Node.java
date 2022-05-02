b552ce19816a022496a3c5351a0bb54ebed972f8
maxshine <maxshine_au@outlook.com>
2022-05-02 11:12:39 +1000

    20220502 solution done with 116

---
+++ src/[116]Populating Next Right Pointers in Each Node.java
@@ -0,0 +1,146 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+
+class Solution {
+    // Mine
+    public Node connectMine(Node root) {
+        Queue<Node> q = new LinkedList<>();
+        if (root == null) {
+            return null;
+        }
+        q.add(root);
+        
+        while (!q.isEmpty()) {
+            int qSize = q.size();
+            Node prev = null;
+            while (qSize>0) {
+                Node current = q.poll();
+                if (prev != null) {
+                    prev.next = current;
+                }
+                prev = current;
+                qSize--;
+                if (current.left != null) {
+                    q.add(current.left);
+                }
+                if (current.right != null) {
+                    q.add(current.right);
+                }
+            }
+        }
+        return root;
+    }
+    // Level Order
+    public Node connectBFS(Node root) {
+
+        if (root == null) {
+            return root;
+        }
+
+        // Initialize a queue data structure which contains
+        // just the root of the tree
+        Queue<Node> Q = new LinkedList<Node>();
+        Q.add(root);
+
+        // Outer while loop which iterates over 
+        // each level
+        while (Q.size() > 0) {
+
+            // Note the size of the queue
+            int size = Q.size();
+
+            // Iterate over all the nodes on the current level
+            for(int i = 0; i < size; i++) {
+
+                // Pop a node from the front of the queue
+                Node node = Q.poll();
+
+                // This check is important. We don't want to
+                // establish any wrong connections. The queue will
+                // contain nodes from 2 levels at most at any
+                // point in time. This check ensures we only 
+                // don't establish next pointers beyond the end
+                // of a level
+                if (i < size - 1) {
+                    node.next = Q.peek();
+                }
+
+                // Add the children, if any, to the back of
+                // the queue
+                if (node.left != null) {
+                    Q.add(node.left);
+                }
+                if (node.right != null) {
+                    Q.add(node.right);
+                }
+            }
+        }
+
+        // Since the tree has now been modified, return the root node
+        return root;
+    }
+    
+    // 2
+    public Node connect(Node root) {
+
+        if (root == null) {
+            return root;
+        }
+
+        // Start with the root node. There are no next pointers
+        // that need to be set up on the first level
+        Node leftmost = root;
+
+        // Once we reach the final level, we are done
+        while (leftmost.left != null) {
+
+            // Iterate the "linked list" starting from the head
+            // node and using the next pointers, establish the 
+            // corresponding links for the next level
+            Node head = leftmost;
+
+            while (head != null) {
+
+                // CONNECTION 1
+                head.left.next = head.right;
+
+                // CONNECTION 2
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
+
+                // Progress along the list (nodes on the current level)
+                head = head.next;
+            }
+
+            // Move onto the next level
+            leftmost = leftmost.left;
+        }
+
+        return root;
+    }
+}
