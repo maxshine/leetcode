e99feff275a871ce3614bfbaa5fbe5ad118b6677
maxshine <maxshine_au@outlook.com>
2022-07-24 09:34:49 +1000

    20220724 solution done with 136

---
+++ src/[136] single-number.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int a = 0;
+        for (int n : nums) {
+            a ^= n;
+        }
+        return a;
+    }
+    // 2 list
+    public int singleNumberList(int[] nums) {
+        List<Integer> no_duplicate_list = new ArrayList<>();
+
+        for (int i : nums) {
+            if (!no_duplicate_list.contains(i)) {
+                no_duplicate_list.add(i);
+            } else {
+                no_duplicate_list.remove(new Integer(i));
+            }
+        }
+        return no_duplicate_list.get(0);
+    }
+    // 3 hash
+    public int singleNumberHashTable(int[] nums) {
+        HashMap<Integer, Integer> hash_table = new HashMap<>();
+
+        for (int i : nums) {
+            hash_table.put(i, hash_table.getOrDefault(i, 0) + 1);
+        }
+        for (int i : nums) {
+            if (hash_table.get(i) == 1) {
+                return i;
+            }
+        }
+        return 0;
+    }
+    // 4 Math
+    public int singleNumberMath(int[] nums) {
+        int sumOfSet = 0, sumOfNums = 0;
+        Set<Integer> set = new HashSet();
+
+        for (int num : nums) {
+            if (!set.contains(num)) {
+                set.add(num);
+                sumOfSet += num;
+            }
+            sumOfNums += num;
+        }
+        return 2 * sumOfSet - sumOfNums;
+    }
+}
