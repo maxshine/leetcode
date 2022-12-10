494a61a76d2d090252e1bcb312c909c825aee25a
maxshine <maxshine_au@outlook.com>
2022-12-11 09:33:58 +1100

    20221205 solution done with 156

---
+++ src/[156]binary-tree-upside-down.java
@@ -0,0 +1,48 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// @solution-sync:begin
+class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        TreeNode curr = root;
+        TreeNode next = null;
+        TreeNode temp = null;
+        TreeNode prev = null;
+
+        while(curr != null) {
+            next = curr.left;
+
+            // swapping nodes now, need temp to keep the previous right child
+            curr.left = temp;
+            temp = curr.right;
+            curr.right = prev;
+
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public TreeNode upsideDownBinaryTreeRecursive(TreeNode root) {
+        if(root == null || root.left == null) {
+            return root;
+        }
+
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;   // node 2 left children
+        root.left.right = root;         // node 2 right children
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+}
